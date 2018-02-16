c7df9315e7e7fd20a53aac99b8232954efeb7cf3
chrisperk <christopherperkins.512@gmail.com>
2018-02-15 19:17:12 -0600

    questions able to be answered and checked, scores accumulating, proceeding to next question working

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Quiz from './components/Quiz/Quiz';
 import './App.css';
 
+let timer;
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,16 +14,19 @@ class App extends Component {
           text: 'What is 2 + 2?',
           answer: 4,
           directions: '(Press 0-9 on keyboard to answer.)',
-          timeRemaining: 10
+          timeRemaining: 10,
+          isUserAnswerCorrect: null
         },
         {
           text: 'What is 4 + 4?',
           answer: 8,
           directions: '(Press 0-9 on keyboard to answer.)',
-          timeRemaining: 10
+          timeRemaining: 10,
+          isUserAnswerCorrect: null
         }
       ],
-      currentQuestion: 0,
+      currentQuestion: null,
+      currentQuestionIndex: 0,
       score: 0,
       userAnswer: null,
       users: []
@@ -30,44 +35,70 @@ class App extends Component {
 
   componentWillMount() {
     document.addEventListener('keyup', this.handleUserAnswer.bind(this));
+    const currentQuestion = this.state.questions[this.state.currentQuestionIndex];
+    this.setState({ currentQuestion });
   }
 
   componentDidMount() {
     fetch('/dilly')
       .then(res => res.json())
       .then(users => this.setState({ users }));
-    setInterval(
+    timer = this.beginTimer();
+    console.log(timer);
+  }
+
+  beginTimer() {
+    return setInterval(
       () => {
-        if (this.state.question.timeRemaining > 0) {
-          this.setState({ 
-            question: {
-              timeRemaining: this.state.question.timeRemaining - 1,
-              text: this.state.question.text,
-              directions: this.state.question.directions,
-              answer: this.state.question.answer
-            }
-          })
+        const currentQuestion = this.state.currentQuestion;
+        if (currentQuestion.timeRemaining > 0) {
+          currentQuestion.timeRemaining = currentQuestion.timeRemaining - 1;
+          this.setState({ currentQuestion });
         }    
       }, 1000);
   }
 
   handleUserAnswer(e) {
-    console.log(this);
+    const currentQuestion = this.state.questions[this.state.currentQuestionIndex];
     const input = parseInt(e.key, 10);
     const keyChoices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     if (!keyChoices.includes(input)) return;
-    if (input === this.state.question.answer) {
+    clearInterval(timer);
+    if (input === currentQuestion.answer) {
       console.log('correct');
+      currentQuestion.isUserAnswerCorrect = true;
+      this.setState({ 
+        currentQuestion,
+        score: this.state.score + (currentQuestion.timeRemaining)
+      });
+      console.log(this.state.score);
+      this.proceedToNextQuestion();
     } else {
       console.log('incorrect');
+      currentQuestion.isUserAnswerCorrect = false;
+      this.setState({ currentQuestion });
+      this.proceedToNextQuestion();
     }
+  }
+
+  proceedToNextQuestion() {
+    setTimeout(() => {
+      if (this.state.currentQuestionIndex < this.state.questions.length - 1) {
+        this.setState({
+          currentQuestionIndex: this.state.currentQuestionIndex + 1
+        });
+        const currentQuestion = this.state.questions[this.state.currentQuestionIndex];
+        this.setState({ currentQuestion });
+        timer = this.beginTimer();
+      }
+    }, 2000);
   }
 
   render() {
     return (
       <div className="App">
         <Quiz 
-          question={this.state.question}
+          question={this.state.currentQuestion}
         />
       </div>
     );
--- client/src/components/Quiz/Quiz.js
+++ client/src/components/Quiz/Quiz.js
@@ -4,7 +4,7 @@ import './Quiz.css';
 const Quiz = props => {
     return (
         <div className="question-wrapper">
-            <main onClick={e => props.onUserAnswer(e)}>
+            <main>
                 <section id="timer">
                     <div id="progress-bar"></div>
                     {props.question.timeRemaining}
@@ -13,8 +13,12 @@ const Quiz = props => {
                     <div id="text">{props.question.text}</div>
                     <div id="directions">{props.question.directions}</div>
                     <div id="result">
-                        <span id="correct"></span>
-                        <span id="incorrect"></span>
+                        <span className={props.question.isUserAnswerCorrect === true ? 'visible' : ''}>
+                            Correct
+                        </span>
+                        <span className={props.question.isUserAnswerCorrect === false ? 'visible' : ''}>
+                            Incorrect
+                        </span>
                     </div>
                 </section>
             </main>
