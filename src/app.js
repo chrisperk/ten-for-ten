f96d692dd8771e62c292fa5c741aa270340a8198
chrisperk <christopherperkins.512@gmail.com>
2018-03-01 17:56:28 -0600

    adjusted port determination

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get('*', (req, res) => {
     res.sendFile('client/build/index.html');
   });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Listening on port:${port}`);
 });
